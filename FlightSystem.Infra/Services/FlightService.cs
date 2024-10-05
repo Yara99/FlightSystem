209fb49c4eb814c92c4c9c2910307e20357d99e8
Yara Haddad <99yarahaddad@gmail.com>
2024-10-05 22:48:09 +0300

    add inheritence

--- FlightSystem.Infra/Services/FlightService.cs
+++ FlightSystem.Infra/Services/FlightService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightSystem.Core.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FlightSystem.Infra.Services
 {
-    public class FlightService
+    public class FlightService : IFlightService
     {
     }
 }
