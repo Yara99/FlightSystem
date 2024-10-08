53c5fe7541c234e8598b54553378ed37db7be3c7
sadeen fadel <sadeenfadel@gmail.com>
2024-10-08 16:32:48 +0300

    city updated

--- FlightSystem/Controllers/CityController.cs
+++ FlightSystem/Controllers/CityController.cs
@@ -53,7 +53,7 @@ namespace FlightSystem.Controllers
         }
         [HttpGet]
         [Route("GetCitiesByCountry")]
-        List<City> GetCitiesByCountry(int countryId)
+       public List<City> GetCitiesByCountry(int countryId)
         {
             return _cityService.GetCitiesByCountry(countryId);
 
